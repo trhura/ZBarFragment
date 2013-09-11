68489cfc23f5e985c4b516fdc7d37a157312276a
Thura Hlaing <trhura@gmail.com>
2013-09-11 16:32:28 +0630

    changed project names

--- examples/ZBarFragmentDemo/src/com/dm/zbar/android/examples/MainActivity.java
+++ examples/ZBarFragmentDemo/src/com/dm/zbar/android/examples/MainActivity.java
@@ -1,18 +1,9 @@
 package com.dm.zbar.android.examples;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.text.TextUtils;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Toast;
-import com.dm.zbar.android.scanner.ZBarConstants;
 import com.dm.zbar.android.scanner.ZBarFragment;
-import net.sourceforge.zbar.Symbol;
 
 public class MainActivity extends FragmentActivity implements ZBarFragment.ResultListener {
 
